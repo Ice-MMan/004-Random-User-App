ad884a8a76151f6ae3fd542e8c6c6584657aea19
Kaan UYSAL <kaanuysal06@gmail.com>
2022-10-21 01:22:10 +0300

    first commit

---
+++ starter/src/App.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+import mailSvg from "./assets/mail.svg";
+import manSvg from "./assets/man.svg";
+import womanSvg from "./assets/woman.svg";
+import manAgeSvg from "./assets/growing-up-man.svg";
+import womanAgeSvg from "./assets/growing-up-woman.svg";
+import mapSvg from "./assets/map.svg";
+import phoneSvg from "./assets/phone.svg";
+import padlockSvg from "./assets/padlock.svg";
+import cwSvg from "./assets/cw.svg";
+import Footer from "./components/footer/Footer";
+import axios from "axios";
+import Adduser from "./components/Adduser";
+
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+
+function App() {
+  const [user, setUser] = useState([]);
+
+  const getusers = async () => {
+    const url = "https://randomuser.me/api/";
+    try {
+      const { data } = await axios(url);
+      // console.log(data.results[0]);
+      setUser(data.results[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getusers();
+  }, []);
+
+  const { picture, email, name, phone, location, dob } = user;
+  console.log(user);
+
+  const [changeuser, setChangeuser] = useState([name]);
+
+  console.log(changeuser);
+
+  return (
+    <main>
+      <div className="block bcg-orange">
+        <img src={cwSvg} alt="cw" id="cw" />
+      </div>
+      <div className="block">
+        <div className="container">
+          <img src={picture?.large} alt="random user" className="user-img" />
+          <p className="user-title">My {changeuser} is</p>
+          <p className="user-value"></p>
+          <div className="values-list">
+            <button
+              className="icon"
+              data-label="name"
+              onClick={() => setChangeuser("name")}
+            >
+              <img src={womanSvg} alt="user" id="iconImg" />
+            </button>
+            <button
+              className="icon"
+              data-label="email"
+              onClick={() => setChangeuser("email")}
+            >
+              <img src={mailSvg} alt="mail" id="iconImg" />
+            </button>
+            <button
+              className="icon"
+              data-label="age"
+              onClick={() => setChangeuser("age")}
+            >
+              <img src={womanAgeSvg} alt="age" id="iconImg" />
+            </button>
+            <button
+              className="icon"
+              data-label="street"
+              onClick={() => setChangeuser("street")}
+            >
+              <img src={mapSvg} alt="map" id="iconImg" />
+            </button>
+            <button
+              className="icon"
+              data-label="phone"
+              onClick={() => setChangeuser("phone")}
+            >
+              <img src={phoneSvg} alt="phone" id="iconImg" />
+            </button>
+            <button
+              className="icon"
+              data-label="password"
+              onClick={() => setChangeuser("password")}
+            >
+              <img src={padlockSvg} alt="lock" id="iconImg" />
+            </button>
+          </div>
+          <div className="btn-group">
+            <button className="btn" type="button" onClick={() => getusers()}>
+              new user
+            </button>
+            <button className="btn" type="button">
+              add user
+            </button>
+          </div>
+
+          <table className="table">
+            <thead>
+              <tr className="head-tr">
+                <th className="th">Firstname</th>
+                <th className="th">Email</th>
+                <th className="th">Phone</th>
+                <th className="th">Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="body-tr">{<Adduser user={user} />}</tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Footer />
+      </div>
+    </main>
+  );
+}
+
+export default App;
